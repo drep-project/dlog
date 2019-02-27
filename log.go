f719633ba078a72697710b56a19874bab24c4f7c
hunjixin <1084400399@qq.com>
2019-02-27 16:33:03 +0800

    commit log file

---
+++ log.go
@@ -0,0 +1,64 @@
+package log
+
+import (
+	"github.com/drep-project/drep-chain/common/fileutil"
+	"github.com/mattn/go-colorable"
+	"github.com/mattn/go-isatty"
+	"io"
+	"os"
+)
+
+var DEBUG = false
+
+var (
+	ostream Handler
+	glogger *GlogHandler
+)
+
+func init() {
+	usecolor := (isatty.IsTerminal(os.Stderr.Fd()) || isatty.IsCygwinTerminal(os.Stderr.Fd())) && os.Getenv("TERM") != "dumb"
+	output := io.Writer(os.Stderr)
+	if usecolor {
+		output = colorable.NewColorableStderr()
+	}
+	ostream = StreamHandler(output, TerminalFormat(usecolor))
+	glogger = NewGlogHandler(ostream)
+}
+
+func SetUp(dataDir string, loglevel int, vmModule string, backtraceAt string) error {
+	if dataDir != "" {
+		if !fileutil.IsDirExists(dataDir) {
+			err := os.MkdirAll(dataDir, 0777)
+			if err != nil {
+				return err
+			}
+		}
+
+		rfh, err := SyncRotatingFileHandler(
+			dataDir,
+			262144,
+			JSONFormatOrderedEx(false, true),
+		)
+		if err != nil {
+			return err
+		}
+		glogger.SetHandler(MultiHandler(ostream, rfh))
+	}
+	glogger.Verbosity(Lvl(loglevel))
+	glogger.Vmodule(vmModule)
+	glogger.BacktraceAt(backtraceAt)
+	Root().SetHandler(glogger)
+	return nil
+}
+
+func SetVerbosity(loglevel Lvl)  {
+	 glogger.Verbosity(loglevel)
+}
+
+func SetVmodule(vmModule string) error {
+	return glogger.Vmodule(vmModule)
+}
+
+func SetBacktraceAt(backtraceAt string) error {
+	return glogger.BacktraceAt(backtraceAt)
+}
